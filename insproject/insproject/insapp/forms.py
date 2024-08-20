ae56b9172ada0676c989567c3124713eef5b6fef
SIVASAKTHI <148606157+mruthi@users.noreply.github.com>
2024-08-20 12:51:18 +0530

    Delete insproject directory

--- insproject/insproject/insapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-
-class insForm(forms.ModelForm):
-    class Meta():
-        model=insModel
-        fields=['age','bmi','children','sex_male','smoker_yes']
